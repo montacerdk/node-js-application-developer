559c53c12c3de0df9fc2f77337b2eae02e099478
Montacer Dkhilali <montacer.dkhilali@unicorne.cloud>
2022-11-01 22:40:22 -0400

    Data types
    Symbols

---
+++ 02-javascript-key-concepts/data-types/index.js
@@ -0,0 +1,42 @@
+const number1 = Number(66);
+const number2 = 66;
+
+const string1 = String("Hello");
+const string2 = "Hello";
+
+const symbol1 = Symbol();
+const symbol2 = Symbol("Hello");
+
+const sym1 = Symbol("Sym1");
+const sym2 = Symbol("Sym2");
+
+console.log(sym1 === sym2); // false.
+
+// const RED = "red";
+// const BLUE = "blue";
+// const ORANGE = "orange";
+// const YELLOW = "yellow";
+const blaBla = "blue";
+
+const RED = Symbol("red");
+const BLUE = Symbol("blue");
+const ORANGE = Symbol("orange");
+const YELLOW = Symbol("yellow");
+
+const getLevelFromColor = (color) => {
+  switch (color) {
+    case RED:
+      return "severe";
+    case ORANGE:
+      return "high";
+    case BLUE:
+      return "low";
+    case YELLOW:
+      return "elevated";
+    default:
+      return "Unkown color.";
+  }
+};
+
+console.log("getLevelFromColor", getLevelFromColor(BLUE));
+console.log("getLevelFromColor", getLevelFromColor(blaBla));
