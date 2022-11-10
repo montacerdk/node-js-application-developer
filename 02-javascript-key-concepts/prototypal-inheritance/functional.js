f1c9c19c2b93785b03995ac05f1c58df5f55dd8a
Montacer Dkhilali <montacer.dkhilali@unicorne.cloud>
2022-11-10 15:04:07 -0500

    Functional Prototypal Inheritance

---
+++ 02-javascript-key-concepts/prototypal-inheritance/functional.js
@@ -0,0 +1,39 @@
+const wolf = {
+  howl: function () {
+    console.log(this.name + ": Awoooooooo");
+  },
+};
+
+const dog = Object.create(wolf, {
+  woof: {
+    value: function () {
+      console.log(this.name + ": Woof Woof");
+    },
+  },
+});
+
+const dan = Object.create(dog, {
+  name: { value: "Dan the dog" },
+});
+
+dan.woof(); // prints "Dan the dog: woof"
+dan.howl(); // prints "Dan the dog: awoooooooo"
+
+console.log(Object.getPrototypeOf(wolf) === Object.prototype); //true
+console.log(Object.getPrototypeOf(dan) === dog); //true
+console.log(Object.getPrototypeOf(dog) === wolf); //true
+
+// The wolf object is a plain JavaScript object, created with the object literal syntax.
+// The prototype of plain JavaScript objects is 'Object.prototype'.
+
+// The Object.create function can take two arguments.
+// The first argument is the desired prototype of the object being created
+
+/**
+  To describe the full prototype chain:
+    - The prototype of dan is dog.
+    - The prototype of dog is wolf.
+    - The prototype of wolf is Object.prototype.
+ */
+
+// The second argument of Object.create is an optional Properties Descriptor object.
