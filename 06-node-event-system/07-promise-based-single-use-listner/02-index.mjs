f79dfe94e2ff5810404d533ec01bd05a7edbe86e
Montacer Dkhilali <montacer.dkhilali@unicorne.cloud>
2023-01-12 00:00:00 -0500

    Add comment

--- 06-node-event-system/07-promise-based-single-use-listner/02-index.mjs
+++ 06-node-event-system/07-promise-based-single-use-listner/02-index.mjs
@@ -20,3 +20,7 @@ try {
   if (err.code !== "ABORT_ERR") throw err;
   console.log("canceled");
 }
+
+// This code will now output canceled every time.
+// Since 'uneventful' never emits pinged, after 5000 milliseconds ac.abort is called, and this causes the signal instance passed to events.once to emit an abort event which triggers events.once to reject the returned promise with an AbortError.
+// We check for the AbortError, rethrowing if the error isn't related to the AbortController. If the error is an AbortError we log out canceled.
